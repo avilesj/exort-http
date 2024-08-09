c4f27acf3eb970beff6b765a9a882f3496989685
avilesj <javiles@mailbox.org>
2024-08-08 23:18:12 -0400

    return valid http response -- change scanner for reader

--- main.go
+++ main.go
@@ -5,26 +5,57 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
+
+type HttpMethod string
+
+const (
+	GET    HttpMethod = "GET"
+	POST   HttpMethod = "POST"
+	PUT    HttpMethod = "PUT"
+	DELETE HttpMethod = "DELETE"
+	PATCH  HttpMethod = "PATCH"
+)
+
+type Request struct {
+	Version *string
+	Path    *string
+	Host    *string
+	Agent   *string
+	Method  HttpMethod
+}
 
 func handleConn(conn net.Conn) {
 	defer conn.Close()
 	fmt.Print("Handle conn")
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		message := scanner.Text()
-		fmt.Printf("Received: %s\n", message)
-		conn.Close()
+	reader := bufio.NewReader(conn)
+	var headers []string
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Println(line)
+		if err != nil {
+			fmt.Printf("Parsing error: %s", err)
+		}
+		line = strings.TrimSpace(line)
+		if line == "" {
+			break
+		}
+		headers = append(headers, line)
 	}
+
+	println(headers)
+	response := "HTTP/1.1 200 OK\r\n"
+	conn.Write([]byte(response))
 }
 
 func main() {
-	fmt.Print("Launching server")
+	fmt.Println("Launching server")
 	ln, err := net.Listen("tcp", ":4000")
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	}
-	fmt.Print("Server if online")
+	fmt.Println("Server if online")
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
